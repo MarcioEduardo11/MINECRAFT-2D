bd07b991f4b2e459713365687b0b472f18ba6ee8
marcio <engs-marcioaraujo@camporeal.edu.br>
2026-06-01 17:05:59 -0300

    final1.0

--- base.cpp
+++ base.cpp
@@ -1,94 +1,24 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
 
-const int LINHAS = 12;
-const int COLUNAS = 21;
-
-char mundo[LINHAS][COLUNAS];
-
-int playerX = 0;
-int playerY = 0;
-
-void gerarMapa() {
-
-    for(int y = 0; y < LINHAS; y++) {
-
-        for(int x = 0; x < COLUNAS; x++) {
-
-            int r = rand() % 100;
-
-            if(r < 15)
-                mundo[y][x] = 'G';
-
-            else if(r < 30)
-                mundo[y][x] = 'P';
-
-            else if(r < 40)
-                mundo[y][x] = 'F';
-
-            else if(r < 50)
-                mundo[y][x] = 'A';
-
-            else if(r < 65)
-                mundo[y][x] = 'T';
-
-            else
-                mundo[y][x] = '.';
-        }
-    }
-
-    mundo[playerY][playerX] = 'X';
-}
-
-void mostrarMapa() {
-
-    for(int y = 0; y < LINHAS; y++) {
-
-        for(int x = 0; x < COLUNAS; x++) {
-
-            cout << mundo[y][x] << " ";
-        }
-
-        cout << endl;
-    }
-}
-
 int main() {
-    int playerX = 0;
-      int playerY = 0;
+    int linhas = 16;
+    int colunas = 16;
 
-    srand(time(0));
+    char mapa [16] [16];
 
-    gerarMapa();
+    for (int i =0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            mapa [i] [j] = '.';
+        }
+    }
 
-    char comando;
 
-    while(true) {
-
-        system("cls");
-
-        mostrarMapa();
-
-        cin >> comando;
-
-        mundo[playerY][playerX] = '.';
-
-        if(comando == 'w' && playerY > 0)
-            playerY--;
-
-        if(comando == 's' && playerY < LINHAS - 1)
-            playerY++;
-
-        if(comando == 'a' && playerX > 0)
-            playerX--;
-
-        if(comando == 'd' && playerX < COLUNAS - 1)
-            playerX++;
-
-        mundo[playerY][playerX] = 'X';
+    for (int i =0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            cout << mapa [i] [j] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
